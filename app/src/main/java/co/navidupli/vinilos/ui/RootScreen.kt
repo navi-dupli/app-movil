3222fcb88123037c757ef9b250e4bd72c8a77973
plinio <p.grijalbar@uniandes.edu.co>
2022-11-04 22:47:15 -0500

    Agregar imagen de background y cambiar estilos de los botones

--- app/src/main/java/co/navidupli/vinilos/ui/RootScreen.kt
+++ app/src/main/java/co/navidupli/vinilos/ui/RootScreen.kt
@@ -1,31 +1,42 @@
 package co.navidupli.vinilos.ui.theme
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import co.navidupli.vinilos.Screen
+import co.navidupli.vinilos.R
+
 
 @Composable
 fun RootScreen(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(bottom = 20.dp),
-        contentAlignment = Alignment.BottomCenter) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            .fillMaxHeight(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Image(
+            contentScale = ContentScale.FillHeight,
+            painter = painterResource(id = R.drawable.image_background_root),
+            contentDescription = null
+        )
+        Column(Modifier.align(Alignment.Center)) {
             Spacer(modifier = Modifier.padding(10.dp))
-            Button(
+            OutlinedButton(
                 onClick = {
-
                     navController.popBackStack()
-
                     navController.navigate(Screen.AppScaffold.route + "/0") {
                         popUpTo(navController.graph.startDestinationId)
                         launchSingleTop = true
@@ -38,7 +49,7 @@ fun RootScreen(navController: NavController) {
                 Text(text = "Visitante", fontSize = 20.sp)
             }
             Spacer(modifier = Modifier.padding(4.dp))
-            Button(
+            OutlinedButton(
                 onClick = {
                     navController.popBackStack()
                     navController.navigate(Screen.AppScaffold.route + "/1") {
@@ -52,6 +63,14 @@ fun RootScreen(navController: NavController) {
             ) {
                 Text(text = "Coleccionista", fontSize = 20.sp)
             }
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(0.8f),
+                text = "VINILOS",
+                fontSize = 50.sp,
+                color = Color.White,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
