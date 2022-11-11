50512890329e283607370b8dbccc88d32007c204
mviviana <m.ojedan@uniandes.edu.co>
2022-11-10 19:25:40 -0500

    Feature/32 add repository form associate track (#54)

--- app/src/main/java/co/navidupli/vinilos/repositories/AlbumRepository.kt
+++ app/src/main/java/co/navidupli/vinilos/repositories/AlbumRepository.kt
@@ -2,9 +2,7 @@ package co.navidupli.vinilos.repositories
 
 import co.navidupli.vinilos.broker.AlbumApiService
 import co.navidupli.vinilos.broker.RetrofitClient
-import co.navidupli.vinilos.model.Album
-import co.navidupli.vinilos.model.AlbumCreate
-import co.navidupli.vinilos.model.AlbumCreated
+import co.navidupli.vinilos.model.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -33,6 +31,27 @@ class AlbumRepository {
                     }
 
                     override fun onResponse(call: Call<AlbumCreated>, response: Response<AlbumCreated>) {
+                        if (response.code() == 400) {
+                            onFailure("Bad request")
+                        } else {
+                            onResponse(response.body()!!)
+                        }
+                    }
+                })
+            return resp
+        }
+
+        fun postAssociateTrackAlbum(body: TrackAsociate,idAlbum: Int?, onResponse:(resp: Track)->Unit, onFailure:(resp:String)->Unit) : String? {
+            val resp: String? = null
+
+            albumWebApi.postAssociateTrackAlbum(body,idAlbum).enqueue(
+
+                object : Callback<Track> {
+                    override fun onFailure(call: Call<Track>, t: Throwable) {
+                        onFailure(t.message!!)
+                    }
+
+                    override fun onResponse(call: Call<Track>, response: Response<Track>) {
                         if (response.code() == 400) {
                             onFailure("Bad request")
                         } else {
--- app/src/main/java/co/navidupli/vinilos/viewModel/AssociateTracksViewModel.kt
+++ app/src/main/java/co/navidupli/vinilos/viewModel/AssociateTracksViewModel.kt
@@ -17,6 +17,11 @@ class AssociateTracksViewModel : ViewModel() {
     val duration: LiveData<String> = _duration
     private val _name = MutableLiveData<String>()
     val name: LiveData<String> = _name
+    private val _statusAssociate = MutableLiveData<Boolean>()
+    val statusAssociate: LiveData<Boolean> = _statusAssociate
+    private val _loadAssociateTrack = MutableLiveData<Boolean>()
+    val loadAssociateTrack: LiveData<Boolean> = _loadAssociateTrack
+
     private var _albums = MutableLiveData<List<Album>>(listOf())
     var albums: LiveData<List<Album>> = _albums
 
@@ -35,7 +40,17 @@ class AssociateTracksViewModel : ViewModel() {
             name = _name.value
         )
         val idAlbum =  _idAlbum.value
-        // to do add repository
+
+        AlbumRepository.postAssociateTrackAlbum(track,idAlbum,
+            onResponse = {
+                _statusAssociate.value = true
+                _loadAssociateTrack.value = true
+                clearState()
+            }, onFailure = {
+                _statusAssociate.value = false
+                _loadAssociateTrack.value = true
+            })
+
     }
 
     fun setIdAlbum(value: Int){
@@ -49,6 +64,16 @@ class AssociateTracksViewModel : ViewModel() {
     fun setNameTrack(value: String){
         _name.value = value
     }
+
+
+    fun setloadAssociateTrack(value: Boolean) {
+        _loadAssociateTrack.value = value
+    }
+
+    fun setStatusAssociateTrackAlbum() {
+        _statusAssociate.value = null
+    }
+
 
 
 
