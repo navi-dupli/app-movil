5931d65dfd960019c3b1b5047409824a06495b22
Viviana Ojeda <m.ojedan@uniandes.edu.co>
2022-11-13 22:09:58 -0500

    add service artist

---
+++ app/src/main/java/co/navidupli/vinilos/broker/CollectorApiService.kt
@@ -0,0 +1,24 @@
+package co.navidupli.vinilos.broker
+
+import co.navidupli.vinilos.model.*
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+
+interface AlbumApiService {
+
+    @GET("/albums")
+    suspend fun getAlbums(): List<Album>
+
+    @POST("albums")
+    fun postAlbum(@Body params: AlbumCreate): Call<AlbumCreated>
+
+    @POST("/albums/{id_album}/tracks")
+    fun postAssociateTrackAlbum( @Body params: TrackAsociate, @Path("id_album") idAlbum: Long?): Call<Track>
+
+    @GET("/albums/{album_id}")
+    fun getAlbumDetail(@Path("album_id") album_id: Int): Call<Album>
+}
