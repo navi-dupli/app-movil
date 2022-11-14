94814eea5831d7873545f028febe6d5b946081f6
mviviana <m.ojedan@uniandes.edu.co>
2022-11-14 08:38:51 -0500

    20 add repository list performers (#61)
    * add viewModel
    
    * add service artist
    
    * collector Api and Repository

---
+++ app/src/main/java/co/navidupli/vinilos/repositories/CollectorRepository.kt
@@ -0,0 +1,28 @@
+package co.navidupli.vinilos.repositories
+
+import co.navidupli.vinilos.broker.CollectorApiService
+import co.navidupli.vinilos.broker.RetrofitClient
+import co.navidupli.vinilos.model.*
+
+class CollectorRepository {
+    companion object {
+        private var collectorWebApi: CollectorApiService =
+            RetrofitClient.createRetrofitClient().create(CollectorApiService::class.java)
+
+
+        var collectors: List<Collector> = listOf()
+        var performersCollector: List<Performer> = listOf()
+
+        suspend fun getCollectors(): List<Collector> {
+            collectors = collectorWebApi.getCollectors()
+            return collectors
+        }
+
+        suspend fun getPerformersCollector(idCollector : Int): List<Performer> {
+            performersCollector = collectorWebApi.getAllPerformersCollector(idCollector)
+            return performersCollector
+        }
+
+    }
+
+}
