bb750fffdcfa67a0f0ae39d65ae09096559f5b3f
Duvan Jamid Vargas <dj.vargasc1@uniandes.edu.co>
2022-11-16 20:30:10 +0100

    se borra import inutilizado

--- app/src/main/java/co/navidupli/vinilos/components/DetailComponent.kt
+++ app/src/main/java/co/navidupli/vinilos/components/DetailComponent.kt
@@ -1,7 +1,6 @@
 package co.navidupli.vinilos.components
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
